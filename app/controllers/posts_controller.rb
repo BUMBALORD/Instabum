c7b830a0cb9333a9f74d7dbb884f851f4734cd9a
DAVID HALLINAN <davidhallinan@DAVIDs-MacBook-Pro-2.local>
2016-02-05 12:59:44 -0800

    DRY up a bit of code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,28 +10,45 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your Instabum post has been created!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Your Instabum post could not be created. Please check that you have entered all the necessary criteria"
+      render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      flash[:success] = "Your Instabum post has been updated!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Your Instabum post could not be updated. Please check that you have entered all the necessary criteria"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Your Instabum post has been deleted!"
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
