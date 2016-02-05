8b780ca1ac1c82467f40803075ffb0f645efe241
DAVID HALLINAN <davidhallinan@DAVIDs-MacBook-Pro-2.local>
2016-02-04 17:06:48 -0800

    full CRUD routes in place

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,23 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.create(post_params)
     redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
   end
 
   private
